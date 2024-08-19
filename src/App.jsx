f0d576ee3139bfffe94c7791de8bf9c93ae87972
Mahijeet Reddy <mahijeetreddy@gmail.com>
2024-08-19 15:17:08 -0700

    minor react app fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Portfolio = () => {
   return (
